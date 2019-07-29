db594553533db5208c81c876e0f038c697d3b2ad
Blake <bshepherd2019@gmail.com>
2019-07-29 10:25:36 -0700

    Added Demo.py

---
+++ demo.py
@@ -0,0 +1,20 @@
+# -------------------------------------------------
+#        Name: Blake Shepherd 
+#    Filename: demo.py
+#        Date: July 29th, 2019 
+#
+# Description: Demo for Github
+#               
+# -------------------------------------------------
+
+def main():
+
+    name = "Blake"
+
+    print("Hello, my name is" , name)
+
+
+
+
+if __name__ == "__main__":
+    main() 
